1ffcbfa29e3110ff90521378611d93437f44eb67
akramnik <akramnik@gmail.com>
2012-09-19 21:04:44 -0500

    json!

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <boost/mpl/for_each.hpp>
 #include <boost/mpl/size.hpp>
 #include <boost/fusion/include/value_at.hpp>
+#include <boost/fusion/include/at.hpp>
 #include <type_traits>
 #include <iostream>
 
@@ -17,54 +18,42 @@ BOOST_FUSION_DEFINE_STRUCT(
     (int, z)
     (Foo, foo))
 
-struct Printer {
-    template<typename T>
-    typename std::enable_if<boost::fusion::traits::is_sequence<T>::value>::type
-    operator()(const T & val) const {
-	boost::fusion::for_each(val, *this);
-    }
-
-    template<typename T>
-    typename std::enable_if<std::is_integral<T>::value>::type
-    operator()(const T & val) const {
-	std::cout << val << std::endl;
-    }
-};
-
 template<template<typename> class Func>
 struct Helper {
     template<typename T>
     static typename std::enable_if<boost::fusion::traits::is_sequence<T>::value>::type
-    print(const char *name, T*) {
-	std::cout << name << " sequence" << std::endl;	
-	
+    print(const char *name, const T & val) {
 	typedef boost::mpl::range_c<int, 0, boost::mpl::size<T>::value> range;
-	boost::mpl::for_each<range>(Func<T>());
+	boost::mpl::for_each<range>(Func<T>(val));
     }
 
     template<typename T>
     static typename std::enable_if<std::is_integral<T>::value>::type
-    print(const char *name, T*) {
-	std::cout << name << " integral" << std::endl;
+    print(const char *name, const T & val) {
+	std::cout << name << ":" << val << ", ";
     }
 };
 
 template<typename T>
 struct Func {
+    const T & val;
+
+    Func(const T & val) : val(val) { }
+
     template<typename N>
     void operator()(N) const {
 	const char *name = boost::fusion::extension::struct_member_name<T, N::value>::call();
-	typename boost::fusion::result_of::value_at<T, N>::type *ptr = NULL;
-	Helper<Func>::print(name, ptr);
+	auto &child = boost::fusion::at<N>(val);
+	Helper<Func>::print(name, child);       
     }
 };
 
 template<typename T>
 void doit(const T & val) {
-    boost::fusion::for_each(val, Printer());
-
+    std::cout << "{ ";
     typedef boost::mpl::range_c<int, 0, boost::mpl::size<T>::value> range;
-    boost::mpl::for_each<range>(Func<T>());
+    boost::mpl::for_each<range>(Func<T>(val));
+    std::cout << "} ";
 }
 
 int main(int argc, char ** argv) {
